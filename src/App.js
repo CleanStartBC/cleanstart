17b14b060f8cbcb212b9e9141ba0512737025695
CleanStartBC <cleanstartdeveloper@gmail.com>
2019-06-22 23:51:42 -0700

    fixed preview

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const App = (props) => (
   <Router>
     <ScrollToTop>
       <Switch>
+        <Route exact path="/preview" render={routeProps => <AsyncPreview {...routeProps} />}/>
         <Route exact path='/' render={() => <AsyncHome prismicCtx={props.prismicCtx}/>}/>
         <Route exact path="/:uid" render={routeProps => <AsyncPage {...routeProps} prismicCtx={props.prismicCtx} />} />
         <Route exact path="/blog/:uid" render={routeProps => <AsyncPost {...routeProps} prismicCtx={props.prismicCtx} />} />
-        <Route exact path="/preview" render={routeProps => <AsyncPreview {...routeProps} />}/>
       </Switch>
     </ScrollToTop>
   </Router>
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,6 +3,8 @@ import qs from 'qs';
 import Prismic from 'prismic-javascript';
 import PrismicConfig from '../prismic-configuration';
 
+const apiEndpoint = 'http://cleanstart.prismic.io/api/v2'
+
 export default class Preview extends React.Component {
 
   componentWillMount() {
@@ -11,7 +13,7 @@ export default class Preview extends React.Component {
 
   preview(props) {
     const params = qs.parse(this.props.location.search.slice(1))
-    Prismic.getApi(PrismicConfig.apiEndpoint)
+    Prismic.getApi(apiEndpoint)
       .then(api => { api.previewSession(params.token, PrismicConfig.linkResolver, '/')
       .then(url => {
         props.history.push(url);
