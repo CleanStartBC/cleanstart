89a341e1fbd0d6c927737002e5073e519cde4c19
CleanStartBC <cleanstartdeveloper@gmail.com>
2019-06-24 16:10:35 -0700

    changes to react helmet

--- src/meta.js
+++ src/meta.js
@@ -6,4 +6,11 @@ export default document =>
   <Helmet>
     <title>{RichText.asText(document.meta_title) || 'CleanStart'}</title>
     {document.meta_description ? <meta name="description" content={RichText.asText(document.meta_description)} /> : <meta name="description" content='CleanStart - Professional Junk and Rubbish Removal Services' /> }
+
+    {document.og_image && <meta property="og:image" content={document.og_image.url} />}
+    {document.og_title && <meta property="og:title" content={RichText.asText(document.og_title)} />}
+    {document.og_description && <meta property="og:description" content={RichText.asText(document.og_description)} />}
+
+
+
   </Helmet>
